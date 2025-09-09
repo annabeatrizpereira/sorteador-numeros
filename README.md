db629ff286db6bead3c43ab85eb07938048a34b7
Anna Beatriz <annabeatrizp999@gmail.com>
2025-09-09 17:47:52 -0300

    feito ajustes no layout, dado início ao javascript

---
+++ README.md
@@ -0,0 +1,5 @@
+## Funcionalidades da aplicação
+
+- O usuário pode digitar quantos números quer sortear em um primeiro campo;
+- O usuário pode escolher um intervalo entre dois números para esse sorteio;
+- Ao sortear os números devem aparecer, um por um, de maneira animada na tela.
--- scripts.js
+++ scripts.js
@@ -1,1 +1,33 @@
-console.log("teste")
+const btnGoBack = document.querySelector("#result button")
+const btnSort = document.querySelector("#sort button");
+const sectionSort = document.getElementById("sort");
+const sectionResult = document.getElementById("result");
+const inputNumbers = document.getElementById("numbers");
+const inputFrom = document.getElementById("from");
+const inputTo = document.getElementById("to");
+const form = document.querySelector("form");
+
+// alternando para a tela do resultado
+form.addEventListener("submit", (e) => {
+    // não recarregar a página on submit
+    e.preventDefault();
+
+    // valida se todos os campos estão preenchidos
+    if((inputNumbers.value.length <= 0) || (inputFrom.value.length <= 0) || (inputFrom.value.length <= 0)){
+        alert("Preencha todos os campos para sortear!");
+    }else {
+        console.log(inputNumbers.value)
+        console.log(inputFrom.value)
+        console.log(inputTo.value)
+
+        sectionSort.classList.add("hidden");
+        sectionResult.classList.remove("hidden");
+    }
+
+})
+
+// alternando para a tela inicial
+btnGoBack.addEventListener("click", () => {
+    sectionSort.classList.remove("hidden");
+    sectionResult.classList.add("hidden");
+})
